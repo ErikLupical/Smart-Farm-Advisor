8544886a6183ec74281f66897e954670f753e3c9
= <erikbartholemew@gmail.com>
2025-04-17 21:16:37 +0700

    plant envoronment data published to b6610546312/plant_sensors

--- collection.py
+++ collection.py
@@ -1,9 +1,12 @@
-from machine import Pin, ADC
+from machine import Pin, I2C, ADC
 from time import sleep
 import dht
+import json
 import network
 from umqtt.robust import MQTTClient
 from config import WIFI_SSID, WIFI_PASS, MQTT_BROKER, MQTT_USER, MQTT_PASS
+
+TOPIC = 'b6610546312/plant_sensors'
 
 led_wifi = Pin(2, Pin.OUT)
 led_wifi.value(1)  # turn the red led off
@@ -27,20 +30,25 @@ led_iot.value(0)   # turn the green led on
 def res_to_lux(resistance):
     return 562.3413/(resistance*0.001) ** 1.25
 
+t_sensor = I2C(1, sda=Pin(4), scl=Pin(5))
+t_sensor.writeto(77, bytearray([0x04, 0b01100000]))
+t_sensor.writeto(77, bytearray([0]))
 h_sensor = dht.DHT11(Pin(32))
-s_sensor = ADC(Pin(34))
 l_sensor = ADC(Pin(36))
+s_sensor = ADC(Pin(34))
+s_sensor.width(ADC.WIDTH_12BIT)   # set 12 bit return values (returned range 0-4095)     
 
 while True:
   try:
-    sleep(2)
+    sleep(1)
     
     h_sensor.measure()
-    
+
     # temperature
-    temp = h_sensor.temperature()
-    temp_f = temp * (9/5) + 32.0
-    print('Temperature: %3.1f C' %temp)
+    temp = t_sensor.readfrom(77, 2)
+    temp_c = (256*temp[0] + temp[1])/128
+    temp_f = value * (9/5) + 32.0
+    print('Temperature: %3.1f C' %temp_c)
     print('Temperature: %3.1f F' %temp_f)
     
     # humidity
@@ -55,7 +63,22 @@ while True:
     light = res_to_lux(R_LDR)
     print('Light: %3.1f lux' %light)
     
+    # soil
+    soil = s_sensor.read()
+    soil_percent = (1 - (soil / 4095.0)) * 100
+
+    print('Soil: %3.1f %%' %soil_percent)
+    
     print()
     
+    data = {
+        'temperature_c': temp_c,
+        'temperature_f': temp_f,
+        'humidity':hum,
+        'light': light,
+        'moisture': soil_percent,
+        }
+    mqtt.publish(TOPIC, json.dumps(data))
+
   except OSError as e:
     print('Failed to read sensor.')
